0ce78120d0e5f83f0f2080aa5d7474e186b4707d
Tazmin Sacara <sacaraat@gmail.com>
2026-04-07 11:35:55 -0300

    Refactor code structure for improved readability and maintainability

---
+++ creaerte page/galeria-assets.js
@@ -0,0 +1,422 @@
+window.GALERIA_ASSETS = {
+  "2022 Murga del Tomate (3).webp": {
+    "thumb": "./talleres/thumbs/2022 Murga del Tomate (3).webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "CUADRADO2.webp": {
+    "thumb": "./talleres/thumbs/CUADRADO2.webp",
+    "width": 4288,
+    "height": 2848
+  },
+  "DAMIPINTANDO.webp": {
+    "thumb": "./talleres/thumbs/DAMIPINTANDO.webp",
+    "width": 4498,
+    "height": 2978
+  },
+  "DIBUJANDO.webp": {
+    "thumb": "./talleres/thumbs/DIBUJANDO.webp",
+    "width": 2256,
+    "height": 1496
+  },
+  "DIBUJANDO2.webp": {
+    "thumb": "./talleres/thumbs/DIBUJANDO2.webp",
+    "width": 2256,
+    "height": 1496
+  },
+  "EUROPA.webp": {
+    "thumb": "./talleres/thumbs/EUROPA.webp",
+    "width": 3216,
+    "height": 2136
+  },
+  "EUROPA2.webp": {
+    "thumb": "./talleres/thumbs/EUROPA2.webp",
+    "width": 3216,
+    "height": 2136
+  },
+  "Ensamble.webp": {
+    "thumb": "./talleres/thumbs/Ensamble.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "Entrenamiento.webp": {
+    "thumb": "./talleres/thumbs/Entrenamiento.webp",
+    "width": 2000,
+    "height": 3008
+  },
+  "FIESTA.webp": {
+    "thumb": "./talleres/thumbs/FIESTA.webp",
+    "width": 3008,
+    "height": 2000
+  },
+  "Fractales.webp": {
+    "thumb": "./talleres/thumbs/Fractales.webp",
+    "width": 4288,
+    "height": 2848
+  },
+  "HISTORICA2.webp": {
+    "thumb": "./talleres/thumbs/HISTORICA2.webp",
+    "width": 2082,
+    "height": 1470
+  },
+  "JACOBACCI.webp": {
+    "thumb": "./talleres/thumbs/JACOBACCI.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "JACOBACCI1.webp": {
+    "thumb": "./talleres/thumbs/JACOBACCI1.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "MONTAÑAS.webp": {
+    "thumb": "./talleres/thumbs/MONTAÑAS.webp",
+    "width": 3216,
+    "height": 2136
+  },
+  "Murga del Tomate.webp": {
+    "thumb": "./talleres/thumbs/Murga del Tomate.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "Murga.webp": {
+    "thumb": "./talleres/thumbs/Murga.webp",
+    "width": 4288,
+    "height": 2848
+  },
+  "OBRAS1.webp": {
+    "thumb": "./talleres/thumbs/OBRAS1.webp",
+    "width": 6016,
+    "height": 4016
+  },
+  "PASEO.webp": {
+    "thumb": "./talleres/thumbs/PASEO.webp",
+    "width": 3024,
+    "height": 4032
+  },
+  "Peña.webp": {
+    "thumb": "./talleres/thumbs/Peña.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "SONRIENDO.webp": {
+    "thumb": "./talleres/thumbs/SONRIENDO.webp",
+    "width": 3648,
+    "height": 5472
+  },
+  "SUM.webp": {
+    "thumb": "./talleres/thumbs/SUM.webp",
+    "width": 2256,
+    "height": 1496
+  },
+  "Tony Mora. Autoretrato. 0,61 x 0,62 Acrilico 2007.webp": {
+    "thumb": "./talleres/thumbs/Tony Mora. Autoretrato. 0,61 x 0,62 Acrilico 2007.webp",
+    "width": 4032,
+    "height": 3024
+  },
+  "artexigual1.webp": {
+    "thumb": "./talleres/thumbs/artexigual1.webp",
+    "width": 4032,
+    "height": 3024
+  },
+  "artexigual2.webp": {
+    "thumb": "./talleres/thumbs/artexigual2.webp",
+    "width": 3024,
+    "height": 4032
+  },
+  "artexigual3.webp": {
+    "thumb": "./talleres/thumbs/artexigual3.webp",
+    "width": 4032,
+    "height": 3024
+  },
+  "artexigual5.webp": {
+    "thumb": "./talleres/thumbs/artexigual5.webp",
+    "width": 4288,
+    "height": 2848
+  },
+  "artexigual6.webp": {
+    "thumb": "./talleres/thumbs/artexigual6.webp",
+    "width": 6016,
+    "height": 4016
+  },
+  "artexigual7.webp": {
+    "thumb": "./talleres/thumbs/artexigual7.webp",
+    "width": 6016,
+    "height": 4016
+  },
+  "banner.webp": {
+    "thumb": "./talleres/thumbs/banner.webp",
+    "width": 1440,
+    "height": 959
+  },
+  "batik.webp": {
+    "thumb": "./talleres/thumbs/batik.webp",
+    "width": 653,
+    "height": 513
+  },
+  "carnaval2.webp": {
+    "thumb": "./talleres/thumbs/carnaval2.webp",
+    "width": 1440,
+    "height": 1908
+  },
+  "cine.webp": {
+    "thumb": "./talleres/thumbs/cine.webp",
+    "width": 4032,
+    "height": 3024
+  },
+  "cuadrado.webp": {
+    "thumb": "./talleres/thumbs/cuadrado.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "danza.webp": {
+    "thumb": "./talleres/thumbs/danza.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "escultura.webp": {
+    "thumb": "./talleres/thumbs/escultura.webp",
+    "width": 1440,
+    "height": 1800
+  },
+  "grupal.webp": {
+    "thumb": "./talleres/thumbs/grupal.webp",
+    "width": 4624,
+    "height": 2600
+  },
+  "lucestalleres.webp": {
+    "thumb": "./talleres/thumbs/lucestalleres.webp",
+    "width": 3456,
+    "height": 5184
+  },
+  "marisa.webp": {
+    "thumb": "./talleres/thumbs/marisa.webp",
+    "width": 1440,
+    "height": 886
+  },
+  "mujer_pintando.webp": {
+    "thumb": "./talleres/thumbs/mujer_pintando.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "mujeres_aplaude.webp": {
+    "thumb": "./talleres/thumbs/mujeres_aplaude.webp",
+    "width": 1440,
+    "height": 1440
+  },
+  "slide3.jpg.webp": {
+    "thumb": "./talleres/thumbs/slide3.jpg.webp",
+    "width": 1504,
+    "height": 1004
+  },
+  "talleres10.webp": {
+    "thumb": "./talleres/thumbs/talleres10.webp",
+    "width": 3456,
+    "height": 5184
+  },
+  "talleres11.webp": {
+    "thumb": "./talleres/thumbs/talleres11.webp",
+    "width": 2000,
+    "height": 3008
+  },
+  "talleres12.webp": {
+    "thumb": "./talleres/thumbs/talleres12.webp",
+    "width": 3008,
+    "height": 2000
+  },
+  "talleres13.webp": {
+    "thumb": "./talleres/thumbs/talleres13.webp",
+    "width": 1485,
+    "height": 2048
+  },
+  "talleres14.webp": {
+    "thumb": "./talleres/thumbs/talleres14.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres15.webp": {
+    "thumb": "./talleres/thumbs/talleres15.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres16.webp": {
+    "thumb": "./talleres/thumbs/talleres16.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres17.webp": {
+    "thumb": "./talleres/thumbs/talleres17.webp",
+    "width": 3648,
+    "height": 5472
+  },
+  "talleres19.webp": {
+    "thumb": "./talleres/thumbs/talleres19.webp",
+    "width": 3648,
+    "height": 5472
+  },
+  "talleres2.webp": {
+    "thumb": "./talleres/thumbs/talleres2.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres20.webp": {
+    "thumb": "./talleres/thumbs/talleres20.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres21.webp": {
+    "thumb": "./talleres/thumbs/talleres21.webp",
+    "width": 720,
+    "height": 958
+  },
+  "talleres22.webp": {
+    "thumb": "./talleres/thumbs/talleres22.webp",
+    "width": 717,
+    "height": 958
+  },
+  "talleres23.webp": {
+    "thumb": "./talleres/thumbs/talleres23.webp",
+    "width": 718,
+    "height": 941
+  },
+  "talleres24.webp": {
+    "thumb": "./talleres/thumbs/talleres24.webp",
+    "width": 4011,
+    "height": 2876
+  },
+  "talleres25.webp": {
+    "thumb": "./talleres/thumbs/talleres25.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres26.webp": {
+    "thumb": "./talleres/thumbs/talleres26.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres27.webp": {
+    "thumb": "./talleres/thumbs/talleres27.webp",
+    "width": 3648,
+    "height": 5472
+  },
+  "talleres28.webp": {
+    "thumb": "./talleres/thumbs/talleres28.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres29.webp": {
+    "thumb": "./talleres/thumbs/talleres29.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres3.webp": {
+    "thumb": "./talleres/thumbs/talleres3.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres30.webp": {
+    "thumb": "./talleres/thumbs/talleres30.webp",
+    "width": 3072,
+    "height": 2304
+  },
+  "talleres31.webp": {
+    "thumb": "./talleres/thumbs/talleres31.webp",
+    "width": 2008,
+    "height": 3008
+  },
+  "talleres32.webp": {
+    "thumb": "./talleres/thumbs/talleres32.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres33.webp": {
+    "thumb": "./talleres/thumbs/talleres33.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres34.webp": {
+    "thumb": "./talleres/thumbs/talleres34.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres35.webp": {
+    "thumb": "./talleres/thumbs/talleres35.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "talleres36.webp": {
+    "thumb": "./talleres/thumbs/talleres36.webp",
+    "width": 2304,
+    "height": 3456
+  },
+  "talleres37.webp": {
+    "thumb": "./talleres/thumbs/talleres37.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres38.webp": {
+    "thumb": "./talleres/thumbs/talleres38.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres39.webp": {
+    "thumb": "./talleres/thumbs/talleres39.webp",
+    "width": 5472,
+    "height": 3072
+  },
+  "talleres4.webp": {
+    "thumb": "./talleres/thumbs/talleres4.webp",
+    "width": 3008,
+    "height": 2000
+  },
+  "talleres40.webp": {
+    "thumb": "./talleres/thumbs/talleres40.webp",
+    "width": 5472,
+    "height": 3072
+  },
+  "talleres41.webp": {
+    "thumb": "./talleres/thumbs/talleres41.webp",
+    "width": 3008,
+    "height": 2008
+  },
+  "talleres42.webp": {
+    "thumb": "./talleres/thumbs/talleres42.webp",
+    "width": 2256,
+    "height": 1496
+  },
+  "talleres5.webp": {
+    "thumb": "./talleres/thumbs/talleres5.webp",
+    "width": 3008,
+    "height": 2000
+  },
+  "talleres6.webp": {
+    "thumb": "./talleres/thumbs/talleres6.webp",
+    "width": 2048,
+    "height": 1274
+  },
+  "talleres7.webp": {
+    "thumb": "./talleres/thumbs/talleres7.webp",
+    "width": 4032,
+    "height": 3024
+  },
+  "talleres8.webp": {
+    "thumb": "./talleres/thumbs/talleres8.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "talleres9.webp": {
+    "thumb": "./talleres/thumbs/talleres9.webp",
+    "width": 5472,
+    "height": 3648
+  },
+  "telar.webp": {
+    "thumb": "./talleres/thumbs/telar.webp",
+    "width": 5184,
+    "height": 3456
+  },
+  "tiempo de amar.webp": {
+    "thumb": "./talleres/thumbs/tiempo de amar.webp",
+    "width": 5184,
+    "height": 3456
+  }
+};
